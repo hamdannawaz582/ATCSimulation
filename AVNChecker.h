a9b14543aa58fe0cd7151be89b2de01d54a70652
Areeb <areebfaisal49@gmail.com>
2025-05-07 20:44:24 +0500

    Made Logs for Airline Fines

--- AVNChecker.h
+++ AVNChecker.h
@@ -2,19 +2,68 @@
 // Created by Hamdan Nawaz on 07/05/2025.
 //
 #include <fcntl.h>
+#include <filesystem>
+#include <fstream>
 #include <sstream>
 #include <string>
+#include <ctime>
+#include <vector>
 #ifndef AVNCHECKER_H
 #define AVNCHECKER_H
 
 void fineAirline() {
     std::cout << "Fine" << std::endl;
 }
+int AVNID = 0;
 
-void checkAVN(int recievefd) {
+void AddNotice(std::vector<std::string> result) {
+    std::string airline, ID, type, speed,phase;
+    airline = result[0];
+
+
+    std::time_t now = std::time(nullptr);
+    std::tm* local_time = std::localtime(&now);
+
+    ID = result[1];
+    type = result[2];
+    speed = result[3];
+
+    std::ofstream file(airline.c_str(), std::ios::app);
+    file << AVNID++ << std::endl;
+    file << airline << std::endl;
+    file << ID << std::endl;
+    file << type << std::endl;
+    file << speed << std::endl;
+    file << result[5] << std::endl;
+    file << std::put_time(local_time, "%Y-%m-%d %H:%M:%S") << std::endl;
+    local_time->tm_mday += 3;
+    std::mktime(local_time);
+    file << std::put_time(local_time, "%Y-%m-%d") << std::endl;
+    long double Fine = 0;
+    if (type == "Commercial")
+        Fine = 500000;
+    else if (type == "Cargo")
+        Fine = 700000;
+    Fine *= 1.15;
+    file << Fine << std::endl;
+    file << "Unpaid" << std::endl << std::endl;
+}
+
+void UpdateStatus(int fineFD) {
+
+
+}
+
+void checkAVN(int recievefd, int fineFD)
+{
+    int AVNID = 0;
     char buffer[1024];
     int flags = fcntl(recievefd, F_GETFL, 0);
     fcntl(recievefd, F_SETFL, flags | O_NONBLOCK);
+
+    if (fork() == 0) {
+        UpdateStatus(fineFD);
+    }
 
     while (true) {
         int bytesRead = read(recievefd, buffer, 1024);
@@ -23,50 +72,62 @@ void checkAVN(int recievefd) {
         }
         buffer[bytesRead] = '\0';
 
-        std::stringstream ss;
-        if (bytesRead > 0) {
-            ss.write(buffer, bytesRead);
-        } else continue;
-        std::string ID, phase;
-        int speed;
-        ss >> ID >> phase >> speed;
+        std::string input = buffer;
+        std::stringstream ss(input);
+        std::string segment;
+        std::vector<std::string> result;
 
-        if (phase == "AtGate") {
-            if (speed <= 10) goto endfine;
-            fineAirline();
-        } else if (phase == "Taxi") {
-            if (speed <= 30) goto endfine;
-            fineAirline();
+        while (std::getline(ss, segment, '/')) {
+            result.push_back(segment);
+        }
 
-        } else if (phase == "TakeoffRoll") {
-            if (speed <= 290) goto endfine;
-            fineAirline();
+        std::string  airline, ID, type, speed,phase;
+        airline = result[0];
+        ID = result[1];
+        type = result[2];
+        speed = result[3];
+        phase = result[4];
 
-        } else if (phase == "Climb") {
-            if (speed <= 463) goto endfine;
-            fineAirline();
+        int ispeed = std::stoi(speed);
 
-        } else if (phase == "Departure") {
-            if (speed <= 900 && speed >= 800) goto endfine;
-            fineAirline();
-
-        } else if (phase == "Holding") {
-            if (speed < 600) goto endfine;
-            fineAirline();
-        } else if (phase == "Approach") {
-            if (speed <= 290 && speed >= 240) goto endfine;
-            fineAirline();
-
-        } else if (phase == "Landing") {
-            if (speed <= 240 && speed >= 30) goto endfine;
-            fineAirline();
+        std::string status = phase;
+        if (status == "At Gate\n" || status == "At Gate") {
+            if (ispeed <= 10) goto endfine;
+            result.push_back(std::to_string(10));
+            AddNotice(result);
+        } else if (status == "Taxi\n" || status == "Taxi") {
+            if (ispeed <= 30) goto endfine;
+            result.push_back(std::to_string(30));
+            AddNotice(result);
+        } else if (status == "Takeoff Roll\n" || status == "Takeoff Roll") {
+            if (ispeed <= 290) goto endfine;
+            result.push_back(std::to_string(290));
+            AddNotice(result);
+        } else if (status == "Climb\n" || status == "Climb") {
+            if (ispeed <= 463) goto endfine;
+            result.push_back(std::to_string(463));
+            AddNotice(result);
+        } else if (status == "Departure\n" || status == "Departure") {
+            if (ispeed <= 900 && ispeed >= 800) goto endfine;
+            result.push_back(std::to_string(800) + '-' + std::to_string(900));
+            AddNotice(result);
+        } else if (status == "Holding\n" || status == "Holding") {
+            if (ispeed < 600) goto endfine;
+            result.push_back(std::to_string(600));
+            AddNotice(result);
+        } else if (status == "Approach\n") {
+            if (ispeed <= 290 && ispeed >= 240) goto endfine;
+            result.push_back(std::to_string(240) + '-' + std::to_string(290));
+            AddNotice(result);
+        } else if (status == "Landing\n") {
+            if (ispeed <= 240 && ispeed >= 30) goto endfine;
+            result.push_back(std::to_string(30) + '-' + std::to_string(240));
+            AddNotice(result);
         }
         endfine:;
     }
-
     exit(0);
 }
-
 
 
 #endif //AVNCHECKER_H
--- main.cpp
+++ main.cpp
@@ -3,21 +3,22 @@
 #include "AVNChecker.h"
 #include "Orchestrator.h"
 
+//  Added all File Descriptors to Orchestrator.h
 int AVNFD[2];
 int FineFD[2];
 int StripePayFD[2];
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
     pipe(AVNFD);
     pipe(FineFD);
     pipe(StripePayFD);
     Orchestrator orchestrator;
-    // orchestrator.AddFlights();
+    orchestrator.fd = AVNFD[1];
     pthread_t FlightSim;
     pthread_create(&FlightSim, NULL, Orchestrator::proceedSimulation, &orchestrator);
     pthread_detach(FlightSim);
-    // orchestrator.proceedSimulation();
     if (fork() == 0) {
         checkAVN(AVNFD[0]);
     }
